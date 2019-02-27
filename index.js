2102ff2da42729e8b1708abc3630c04cf20c2477
Imon Ovbude <imonovbude@gmail.com>
2019-02-27 13:12:02 -0800

    initialized knexfile.js and imported it into cohorts-router file

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const helmet = require('helmet');
 
 const server = express();
 
+const cohortsRouter = require('./router/cohorts-router')
+
 server.use(express.json());
 server.use(helmet());
+
+server.use('/api', cohortsRouter)
+
+server.get('/', (req, res) => {
+    res.send('Server Running On Port 5000')
+})
 
 server.listen(5000, function () {
     console.log('\n*** Listening on http://localhost:5000 ***\n')
